91da21deef0777e758377320d586612d7526a827
ashley <6312276+AshleyMcGee@users.noreply.github.com>
2023-05-09 16:42:41 -0500

    added posts component and finished header component

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleLarge">Top Stories</span>
-        <span className="headerTitleSm">Hand-picked stories just for you.</span>
+        <span className="headerTitleLg">Top Stories</span>
+        <span className="headerTitleSm">A few hand-picked stories just for you, updated daily.</span>
       </div>
     </div>
   )
