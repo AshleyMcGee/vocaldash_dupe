94a66f8daf6df825e81d5f6b725b6b6803d9ae95
ashley <6312276+AshleyMcGee@users.noreply.github.com>
2023-05-11 08:56:41 -0500

    create posts component and start styles

---
+++ src/posts/Posts.jsx
@@ -0,0 +1,15 @@
+import './posts.css'
+
+export default function Posts() {
+  return (
+    <div className="posts">
+        <div className="postsToday">
+            today's posts
+        </div>
+        <div className="postsPrevious">
+            this week's posts
+        </div>
+      
+    </div>
+  )
+}
